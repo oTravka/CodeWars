d88eebf0613240fc39db0d93754f7fe0e513bb4f
oTravka <153914867+oTravka@users.noreply.github.com>
2024-01-21 14:49:58 +0300

    Update code-wars.js

--- code-wars.js
+++ code-wars.js
@@ -11,3 +11,26 @@ var websites = [];*/
 // SOLUTION:
 var websites = [];
 websites.push("codewars");
+
+//Removing Elements
+
+/*Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!*/
+
+
+// SOLUTION:
+
+function removeEveryOther(arr){
+let arrLenght = arr.length;
+  
+for (let i = 1; i < arr.length; i = i+1)
+{
+  console.log(arr[i], i);
+  arr.splice(i, 1);
+  }
+  return arr;
+}
