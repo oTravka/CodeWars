c43ad378e80851fb9a52d54da431c793ad0ffd1e
oTravka <153914867+oTravka@users.noreply.github.com>
2024-01-24 13:32:52 +0300

    Update code-wars.js

--- code-wars.js
+++ code-wars.js
@@ -97,3 +97,49 @@ function getAverage(marks){
   let roundedNumber = Math.floor(number);
   return roundedNumber
 };
+
+/*Mr. Freeze
+____________________
+DESCRIPTION:
+There is an object/class already created called MrFreeze. 
+Mark this object as frozen so that no other changes can be made to it.*/
+
+// SOLUTION:
+Object.freeze(MrFreeze);
+
+/*Basic subclasses - Adam and Eve
+_________________________________
+DESCRIPTION:
+According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
+
+You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). 
+The first object in the array should be an instance of the class Man. 
+The second should be an instance of the class Woman. 
+Both objects have to be subclasses of Human. 
+Your job is to implement the Human, Man and Woman classes.*/
+
+// SOLUTION:
+class God{
+  /**
+   * @returns Human[]
+   */
+    static create(){
+    const Adam = new Man();
+    const Eva = new Woman();
+    return [Adam, Eva]
+      }
+    }
+      class Human{
+        constructor(){};
+      };
+        
+        class Man extends Human{
+           constructor(){
+             super();
+           };
+        };
+        class Woman extends Human{
+          constructor(){
+            super();
+          };
+        };
